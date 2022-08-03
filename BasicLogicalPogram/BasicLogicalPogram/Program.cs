069ef70e2ec977e93d7a01c5563414f3009ba7ac
ravi04ez <ezekie.em@gmail.com>
2022-08-03 08:44:16 +0530

    Wlcm Msg Logical Program

---
+++ BasicLogicalPogram/BasicLogicalPogram/Program.cs
@@ -0,0 +1,8 @@
+﻿using BasicLogicalPogram;
+
+Console.WriteLine("\t\t\t\t###################################################");
+Console.WriteLine("\t\t\t\t\tWELCOME TO C# BASIC LOGICAL PROGRAM");
+Console.WriteLine("\t\t\t\t###################################################");
+Console.ReadKey();
+
+//Fibonacci.FibonacciSeries();
