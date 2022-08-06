92ca59649f149cfde0b5384a201c83bd44f6f3f7
ravi04ez <ezekie.em@gmail.com>
2022-08-06 07:50:19 +0530

    Added Fibonacci Series

--- BasicLogicalPogram/BasicLogicalPogram/Fibonacci.cs
+++ BasicLogicalPogram/BasicLogicalPogram/Fibonacci.cs
@@ -4,13 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BasicLogicalPogram
+namespace BasicLogicalProgram
 {
     public  class Fibonacci
     {
         public static void FibonacciSeries()
         {
-           
+
+            int Num1 = 0;
+            int Num2 = 1;
+            int Num3;
+
+            // Taking Input from User for Length of Fabonacci Series...
+            Console.Write("\nEnter The Nth Length of Fabonacci Series : ");
+            int LengthOfFabonacci = Convert.ToInt32(Console.ReadLine());  // Storing Length of Fabonacci Series...
+
+            //printing first & Second term of Fabonacci Series
+            Console.Write("Fabonacci series of {0}th lengths are {1} {2}", LengthOfFabonacci, Num1, Num2);
+
+            // Defing Logic For Loop to get the Fabonacci Series..
+            for (int i = 2; i < LengthOfFabonacci; i++)
+            {
+                Num3 = Num1 + Num2;
+
+                //printing term of fabonacci series from 3 to nth Length Enter by User..
+                Console.Write(" " + Num3);
+                Num1 = Num2;
+                Num2 = Num3;
+            }
         }
     }
 }
--- BasicLogicalPogram/BasicLogicalPogram/Program.cs
+++ BasicLogicalPogram/BasicLogicalPogram/Program.cs
@@ -1,8 +1,30 @@
-﻿using BasicLogicalPogram;
+﻿using BasicLogicalProgram;
 
 Console.WriteLine("\t\t\t\t###################################################");
 Console.WriteLine("\t\t\t\t\tWELCOME TO C# BASIC LOGICAL PROGRAM");
 Console.WriteLine("\t\t\t\t###################################################");
+
+int Choice;
+
+do
+{
+    Console.WriteLine("\n0.Exit");
+    Console.WriteLine("1.Fibonacci Series");
+    Console.Write("\nPlease Enter Choice of Question Number : ");
+    Choice=Convert.ToInt32(Console.ReadLine());
+
+    switch (Choice)
+    {
+        case 1:
+            Fibonacci.FibonacciSeries();
+            break;
+        default:
+            Console.WriteLine("Please Enter Correct Question Number");
+            break;
+    }
+
+}
+while(Choice!=0);
 Console.ReadKey();
 
 //Fibonacci.FibonacciSeries();
